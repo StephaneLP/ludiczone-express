e93c02ffea72adac6b25fe5f073a76401030a504
StephaneLP <stephanelepajolec@outlook.com>
2023-05-13 22:50:19 +0200

    maj gestion des erreurs

--- controllers/area-type.controller.js
+++ controllers/area-type.controller.js
@@ -2,22 +2,39 @@ const { AreaTypeModel } = require('../db/sequelize');
 const sequelize = require('../db/sequelize')
 
 exports.findAllAreaType = (req, res) => {
+    let msg = ""
+
     AreaTypeModel.findAll()
         .then((element) => {
-            const msg = "La liste des types de loisir a bien été retournée."
-            res.json({ message: msg, data: element })
+            msg = "La liste des types de loisir a bien été retournée."
+            throw new Error('Division par 0 impossible')
+            res.status(200).json({ success: true, message: msg, data: element })
         })
-        .catch(error => {console.error(`Erreur fonction findAllAreaType (AreaTypeModel) : ${error}`)})  
+        .catch((error) => {
+            msg = "Impossible de charger la liste des types de loisir (Erreur 500)."
+            res.status(500).json({ success: false, message: msg, data: error })
+        })  
 }
 
 exports.deleteAreaType = (req, res) => {
     const id = req.params.id
+    let msg = ""
 
     return AreaTypeModel.destroy({
         where: {id: id}
     })
-    .then((el) => {
-        const msg = `L'élément n°${id} a bien été supprimé.`
-        res.json({ message: msg, data: el })
+    .then((element) => {
+        if(element === 1) {
+            msg = `L'élément id : ${id} a bien été supprimé.`
+            res.status(200).json({ success: true, message: msg, data: {} })
+        }
+        else {
+            msg = `Suppression impossible (aucun élément ne correspond à l'id : ${id}).`
+            res.status(404).json({ success: false, message: msg, data: {} })
+        }
+    })
+    .catch((error) => {
+        msg = "Suppression impossible (Erreur 500)."
+        res.status(500).json({ success: false, message: msg, data: error })
     })
 }

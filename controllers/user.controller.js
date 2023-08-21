8f0c9c494df1affac44e1ffa326eb28c8417043b
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-08-21 12:37:22 +0200

    maj

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcrypt")
 const { UserModel } = require('../db/sequelize')
 const { Op, UniqueConstraintError, ValidationError, ForeignKeyConstraintError } = require("sequelize")
-const mailFrom = require("../setting/mail")
+const { sendMail } = require("../utils/mail")
+const jwt = require("jsonwebtoken")
+const privateKey = require("../setting/privateKey")
 
 /*********************************************************
 SIGNUP (inscription)
@@ -9,15 +11,13 @@ SIGNUP (inscription)
 *********************************************************/
 exports.signUp = (req, res) => {
 
-    sendMail ("Inscription site LudicZone", "<h1>LudicZone</h1><p>Ceci est un message.</p><p>Ceci un lien : <a href='http://localhost:3000/'>lien</a>", "ceodrenazerdsq@outlook.com")
+    sendMail("Inscription site LudicZone", "<h1>LudicZone</h1><p>Ceci est un message.</p><p>Ceci un lien : <a href='http://localhost:3000/'>lien</a>", "ceodren@outlook.com")
         .then((info) => {
             console.log(info.accepted[0], info.response);
         })
         .catch((error) => {
             console.log(error);
         })
-
-
 
     console.log("test")
     return res.status(200).json({ status: "SUCCESS", message: "TEST OK" })
@@ -26,25 +26,22 @@ exports.signUp = (req, res) => {
     .then((hash) => {
         req.body.password = hash
         UserModel.create(req.body)
-        .then((element) => {
-            const  msg = `L'utilisateur '${element.nick_name}' a bien été ajouté.`
+            .then((element) => {
+                const  msg = `L'utilisateur '${element.nick_name}' a bien été ajouté.`
 
 
 
 
-
-
-
-            return res.status(200).json({ status: "SUCCESS", message: msg })
-        })
-        .catch(error => {
-            if(error instanceof UniqueConstraintError || error instanceof ValidationError){
-                return res.status(409).json({ status: "ERR_CONSTRAINT", message: error.message })    
-            }        
-            else {
-                return res.status(500).json({ status: "ERR_SERVER", message: error.message })    
-            }
-        })
+                return res.status(200).json({ status: "SUCCESS", message: msg })
+            })
+            .catch(error => {
+                if(error instanceof UniqueConstraintError || error instanceof ValidationError){
+                    return res.status(409).json({ status: "ERR_CONSTRAINT", message: error.message })    
+                }        
+                else {
+                    return res.status(500).json({ status: "ERR_SERVER", message: error.message })    
+                }
+            })
     })
     .catch(error => {
         return res.status(500).json({ status: "ERR_SERVER", message: error.message })    
@@ -53,25 +50,25 @@ exports.signUp = (req, res) => {
 
 
 
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const sendMail = (subject, message, dest) => {
-    const transport = {
-        service : "hotmail",
-        auth : {
-            user : mailFrom.address,
-            pass : mailFrom.password
-        }
-    }
-    const options = {
-        from : mailFrom.address, 
-        to: dest, 
-        subject: subject, 
-        html: message
-    }
-    const transporter = nodemailer.createTransport(transport)
+// const sendMail = (subject, message, dest) => {
+//     const transport = {
+//         service : "hotmail",
+//         auth : {
+//             user : mailFrom.address,
+//             pass : mailFrom.password
+//         }
+//     }
+//     const options = {
+//         from : mailFrom.address, 
+//         to: dest, 
+//         subject: subject, 
+//         html: message
+//     }
+//     const transporter = nodemailer.createTransport(transport)
 
-    return transporter.sendMail(options)
+//     return transporter.sendMail(options)
     
     // transporter.sendMail(options)
     //     .then((info) => {
@@ -88,6 +85,6 @@ const sendMail = (subject, message, dest) => {
     //     console.log('Email sent: ' + info.response);
     //     }
     // })
-}
+// }
 
 
---
+++ utils/mail.js
@@ -0,0 +1,34 @@
+const mailFrom = require("../setting/mail")
+const nodemailer = require("nodemailer");
+
+const sendMail = (subject, nickName, address) => {
+    const transport = {
+        service : "hotmail",
+        auth : {
+            user : mailFrom.address,
+            pass : mailFrom.password
+        }
+    }
+    const options = {
+        from : mailFrom.address, 
+        to: address, 
+        subject: subject, 
+        html: formatMail(nickName)
+    }
+    const transporter = nodemailer.createTransport(transport)
+
+    return transporter.sendMail(options)
+}
+
+const formatMail = (nickName) => {
+    const html = "<h1>LudicZone"
+    + "<h2>Inscription</h2>"
+    + "<p>Bonjour " + nickName + ","
+    + "<br>Vous venez de vous inscrire sur le site LudicZone."
+    + "<br>Veuillez confirmer votre inscription en cliquant sur le lien suivant :"
+    + "<br><br><a href='http://localhost:3000/'>Lien</a></p>"
+
+    return html
+}
+
+module.exports = { sendMail }

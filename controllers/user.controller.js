a2e64e0e2c3fb08b75ef2404fe0e29bb8fed1969
Stéphane Le Pajolec <stephane.lepajolec@lapiscine.com>
2023-09-14 17:49:00 +0200

    maj

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -118,60 +118,51 @@ POST (SEND NEW MAIL : confirmation de l'asresse email)
 - envoi d'un nouveau token par mail pour confirmer l'adresse mail
 *********************************************************/
 exports.sendNewMail = (req, res) => {
-    const authorizationHeader = req.headers.authorization
+    const email = req.body.email
 
-    if(!authorizationHeader) {
-        const msg = "Un token est nécessaire pour confirmer l'adresse email."
+    if(!email) {
+        const msg = "Veuillez renseigner une adresse email."
         return res.status(400).json({ status: "ERR_REQUEST", message: msg })
     }
 
-    try {
-        const token = authorizationHeader.split(' ')[1]
+    UserModel
+        .findOne({
+            where: { email: email }
+        })
+        .then((element) => {
+            if(!element) {
+                const msg = `Aucun compte n'a été trouvé pour cette adresse email.`
+                return res.status(404).json({ status: "ERR_NOT_FOUND", message: msg })
+            }
 
-        if(token === "null") {
-            const msg = "Un token est nécessaire pour confirmer l'adresse email."
-            return res.status(400).json({ status: "ERR_REQUEST", message: msg })
-        }
+            if(element.verified_email) {
+                const msg = `Cette adresse email a déjà été vérifiée. L'envoi d'un email de confirmation n'est donc pas nécessaire.`
+                return res.status(400).json({ status: "ERR_REQUEST", message: msg })
+            }
 
-        const decoded = jwt.decode(token)
-        const id = decoded.data
+            try {
+                const token = jwt.sign(
+                    { data: element.id }, // Génération du token avec encryptage de l'id user
+                    privateKey,
+                    { expiresIn: "300000" })
 
-        UserModel
-            .findByPk(id)
-            .then((element) => {
-                if(!element) {
-                    const msg = `Aucun utilisateur correspondant à l'id : ${id} n'a été trouvé.`
-                    return res.status(404).json({ status: "ERR_NOT_FOUND", message: msg })
-                }
-    
-                try {
-                    const newtoken = jwt.sign(
-                        { data: element.id }, // Génération du token avec encryptage de l'id user
-                        privateKey,
-                        { expiresIn: "300000" })
-
-                    const url = "http://localhost:3000/inscription-confirm/" + newtoken
-                    sendMailRegistration(element.email, "Inscription site LudicZone", element.nick_name, url)
-                        .then((info) => {
-                            const  msg = `Un mail de validation de la création du compte a été envoyé à l'adresse : '${info.accepted[0]}'.`
-                            res.status(200).json({ status: "SUCCESS", message: msg })
-                        })
-                        .catch((error) => {
-                            const msg = "Une erreur est survenue lors de l'envoi d'un nouveau mail de vérification."
-                            res.status(400).json({ status: "ERR_REQUEST", message: `${msg} (${error.message})` })
-                        })
-                }
-                catch(error) {
-                    const msg = "Une erreur est survenue lors de la génération d'un nouveau token."
-                    res.status(400).json({ status: "ERR_REQUEST", message: msg })
-                }
-            })
-            .catch((error) => {
-                res.status(500).json({ status: "ERR_SERVER", message: error.message })
-            }) 
-    }
-    catch(error) {
-        const msg = "Une erreur est survenue lors de la vérification du token."
-        res.status(400).json({ status: "ERR_REQUEST", message: msg })
-    }
+                const url = "http://localhost:3000/inscription-confirm/" + token
+                sendMailRegistration(element.email, "Inscription site LudicZone", element.nick_name, url)
+                    .then((info) => {
+                        const  msg = `Un mail de validation de la création du compte a été envoyé à l'adresse : '${info.accepted[0]}'.`
+                        res.status(200).json({ status: "SUCCESS", message: msg })
+                    })
+                    .catch((error) => {
+                        const msg = "Une erreur est survenue lors de l'envoi d'un nouveau mail de vérification."
+                        res.status(400).json({ status: "ERR_REQUEST", message: `${msg} (${error.message})` })
+                    })
+            }
+            catch(error) {
+                const msg = "Une erreur est survenue lors de la génération d'un nouveau token."
+                res.status(400).json({ status: "ERR_REQUEST", message: msg })
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ status: "ERR_SERVER", message: error.message })
+        })
 }
